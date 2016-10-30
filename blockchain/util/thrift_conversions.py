5f7c68c90cdb3c9f09e5fc15b32d031fe8ecbfb9
micylt <micylt727@gmail.com>
2016-10-29 18:28:40 -0700

    Testing travis-ci pythonpath.

--- blockchain/util/thrift_conversions.py
+++ blockchain/util/thrift_conversions.py
@@ -33,7 +33,6 @@ __email__ = "joe@dragonchain.org"
 
 import blockchain.gen.messaging.ttypes as message_types
 
-
 # functions below used for converting thrift types to some other structure #
 def thrift_record_to_dict(thrift_record):
     """ returns a dictionary representation of a thrift verification_record """
@@ -153,3 +152,5 @@ def get_verification_record(record):
     thrift_record.signature = convert_to_thrift_signature(record['signature'])
 
     return thrift_record
+
+print "it worked"
