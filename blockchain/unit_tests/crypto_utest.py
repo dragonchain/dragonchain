cad6617bd22ffb01e94789d557d7d00949457290
micylt <micylt727@gmail.com>
2016-11-05 17:57:57 -0700

    Adjusted styling.

--- blockchain/unit_tests/crypto_utest.py
+++ blockchain/unit_tests/crypto_utest.py
@@ -6,8 +6,8 @@ import blockchain.util.crypto as crypto
 
 class TestFinal_hash(unittest.TestCase):
     def test_final_hash(self):
-        testval = crypto.final_hash("Hello World")
-        self.assertEquals(testval,"e63006bd9f35f06cd20582fc8b34ae76a15080297be886decd6dfd42f59e5174a537e8cd92ef577297f967beb6b758c1835f4c270c251e10c12331fcd8635c53")
+        test_val = crypto.final_hash("Hello World")
+        self.assertEquals(test_val, "e63006bd9f35f06cd20582fc8b34ae76a15080297be886decd6dfd42f59e5174a537e8cd92ef577297f967beb6b758c1835f4c270c251e10c12331fcd8635c53")
         self.assertFalse(crypto.final_hash("") == "")
 
 
