cbafbf3a8e47cf6facbf88c54145499f2dbf05fd
Benedetto <benedea@students.wwu.edu>
2016-11-05 17:32:42 -0700

    Added function testing for final_hash in crypto.py

--- blockchain/unit_tests/crypto_utest.py
+++ blockchain/unit_tests/crypto_utest.py
@@ -4,8 +4,8 @@ import blockchain.util.crypto as crypto
 
 class TestFinal_hash(unittest.TestCase):
     def test_final_hash(self):
-        self.fail()
-
+        self.assertEquals(crypto.final_hash("1111111111111111"),"31bca02094eb78126a517b206a88c73cfa9ec6f704c7030d18212cace820f025f00bf0ea68dbf3f3a5436ca63b53bf7bf80ad8d5de7d8359d0b7fed9dbc3ab99")
+        self.assertFalse(crypto.final_hash("") == "")
 
 class TestBytes2long(unittest.TestCase):
     def test_bytes2long(self):
