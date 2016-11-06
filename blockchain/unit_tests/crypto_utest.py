18189562d1ca7d19e2418b035b8bbc93eeefd3e8
micylt <micylt727@gmail.com>
2016-11-05 17:48:54 -0700

    Added some unit test functionality for deterministic_hash.

--- blockchain/unit_tests/crypto_utest.py
+++ blockchain/unit_tests/crypto_utest.py
@@ -1,10 +1,12 @@
 import unittest
+from unittest import TestCase
+
 import blockchain.util.crypto as crypto
 
 
 class TestFinal_hash(unittest.TestCase):
     def test_final_hash(self):
-        self.fail()
+        pass
 
 
 class TestBytes2long(unittest.TestCase):
@@ -13,5 +15,17 @@ class TestBytes2long(unittest.TestCase):
         self.assertRaises(ValueError, crypto.bytes2long, "")
 
         test_val = crypto.bytes2long("111233345556@")
-        self.assertEqual(crypto.bytes2long("111233345556@"), 3897404203108157008445053417024)
+        self.assertEqual(test_val, 3897404203108157008445053417024)
         self.assertTrue(isinstance(test_val, long))
+
+
+class TestDeterministicHash(TestCase):
+    def test_deterministic_hash(self):
+        """ Mock list of prior_block_hash, lower_phase_hash, signatory, signature_ts, public key, block_id, phase,
+            verification_ts, deep_hash(verification_info)
+        """
+        hashed_items = ["123456", "654321", "transaction-service", 1476664195, "xeKuS9t8A=\n-----END PUBLIC KEY-----\n",
+                        "8885196", 1, "origin_id", 1476664210, "553412456235", None]
+
+        val = crypto.deterministic_hash(hashed_items)
+        self.assertEqual(val, 233888947446904696754100748358486582297006536790227845537678672765978391714164843162143)
