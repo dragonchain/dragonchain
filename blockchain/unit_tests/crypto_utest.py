e7f46a100446377373435155e6f78b3e41a487b8
micylt <micylt727@gmail.com>
2016-11-21 20:25:18 -0800

    Made transaction global in crypto unit test and updated travis.yml file to run unit tests.

--- blockchain/unit_tests/crypto_utest.py
+++ blockchain/unit_tests/crypto_utest.py
@@ -34,13 +34,27 @@ from unittest import TestCase
 
 import blockchain.util.crypto as crypto
 
-""" globals """
+""" DISCLAIMER: mock global data for testing purposes. Do not use these keys for anything other than testing. """
 PRIVATE_KEY = "-----BEGIN EC PARAMETERS-----\nBgUrgQQAIQ==\n-----END EC PARAMETERS-----\n-----BEGIN EC PRIVATE KEY-----\nMGgCAQEEHGLBg95ayw1hDWUMsfTdqnlQmVpz3n1vTzr7yhmgBwYFK4EEACGhPAM6\nAATulxsNWAjGOO9hLgJ75xHni8f6aBvk68rA1B+HiIfwbIuMzQultm64qHT3T8Pu\nM4137F4q5L23wA==\n-----END EC PRIVATE KEY-----"
 
 PUBLIC_KEY = "-----BEGIN PUBLIC KEY-----\nME4wEAYHKoZIzj0CAQYFK4EEACEDOgAE7pcbDVgIxjjvYS4Ce+cR54vH+mgb5OvK\nwNQfh4iH8GyLjM0LpbZuuKh090/D7jONd+xeKuS9t8A=\n-----END PUBLIC KEY-----"
 
+TRANSACTION = {'header': {'transaction_id': '8a864b59-46e3-4c9b-8dfd-9d9a2bd4b754',
+                          'transaction_ts': 1479264525,
+                          'actor': 'c26dd972-8683-11e6-977b-3c970e3bee11',
+                          'business_unit': 'a3e13076-8683-11e6-97a9-3c970e3bee11',
+                          'create_ts': 1475180987,
+                          'entity': 'c78f4526-8683-11e6-b1c6-3c970e3bee11',
+                          'family_of_business': 'Test Business Family',
+                          'line_of_business': 'My Business',
+                          'owner': 'Test Node',
+                          'transaction_type': 'TT_REQ'
+                          },
+               'payload': {'action': {'amount': '5.0', 'artifact_id': '12345', 'name': 'Test Payload'},
+                           'source': 'f36c9086-8683-11e6-80dc-3c970e3bee11'}
+               }
+
 SIGNATORY = "31ce807a-868c-11e6-99f6-3c970e3bee11"
-DIGEST = "tvyb6yj6TqmmbpwiCBz9WsGmx6sOJBCvcDkw1GW5jCRWgusILKDWgn5wieDsqWEoKQtfzEgNRI4="
 HASH = 'f3580fb50cbf07432aa2ed87c6737ab180c3d1d387e09b121aead1d98375402e9df52febe72c6fced1c59b59dcad41dde729e84962c2480ce4a4ecf9bd073f16'
 SIG_TS = int(time.time())
 STRIPPED_HASH = "b3a739728d46a011ce9d05705e712044df455b5750ec28c4b08fb6bab689edd21ef9c00be80b872a48fe08e79dc3ebb12e4b6fd1f7278ecaa77cd7c67427edee"
@@ -53,9 +67,9 @@ VERIFICATION_TS = int(time.time())
 
 
 class TestFinalHash(unittest.TestCase):
-    """  testing final_hash() with an arbitrary return value of Hello World to verify it is true
-        as well as testing that if you give function empty string, it still hashes properly """
     def test_final_hash(self):
+        """  testing final_hash() with an arbitrary return value of Hello World to verify it is true
+            as well as testing that if you give function empty string, it still hashes properly """
         test_val = crypto.final_hash("Hello World")
         self.assertEquals(test_val,
                           "e63006bd9f35f06cd20582fc8b34ae76a15080297be886decd6dfd42f59e5174a537e8cd92ef577297f967beb6b758c1835f4c270c251e10c12331fcd8635c53")
@@ -63,8 +77,8 @@ class TestFinalHash(unittest.TestCase):
 
 
 class TestBytes2long(unittest.TestCase):
-    """ test crypto bytes2long """
     def test_bytes2long(self):
+        """ test crypto bytes2long """
         self.assertRaises(AttributeError, crypto.bytes2long, 123)
         self.assertRaises(ValueError, crypto.bytes2long, "")
 
@@ -74,12 +88,8 @@ class TestBytes2long(unittest.TestCase):
 
 
 class TestDeterministicHash(TestCase):
-    """ test crypto deterministic_hash """
     def test_deterministic_hash(self):
-        """ Mock list of prior_block_hash, lower_phase_hash, signatory, signature_ts, public key, block_id, phase,
-            verification_ts, deep_hash(verification_info)
-        """
-
+        """ test crypto deterministic_hash """
         # testing to insure that hashed items may contain a mix of strings, numbers or None types.
         hashed_items = ["123456", "654321", "transaction-service", 1476664195, "xeKuS9t8A=\n-----END PUBLIC KEY-----\n",
                         "8885196", 1, "origin_id", 1476664210, "553412456235", None]
@@ -92,8 +102,8 @@ class TestDeterministicHash(TestCase):
 
 
 class TestSignVerificationRecord(TestCase):
-    """ test crypto sign_verification_record """
     def test_sign_verification_record(self):
+        """ test crypto sign_verification_record """
         verification_info = ""
 
         expected_output = {'phase': PHASE, 'verification_record': {'verification_info': '', 'verification_ts': VERIFICATION_TS, 'block_id': BLOCK_ID,
@@ -110,24 +120,12 @@ class TestSignVerificationRecord(TestCase):
 
 
 class TestAssembleSigBlock(TestCase):
-    """ test crypto assemble_sig_block """
     def test_assemble_sig_block(self):
-        transaction = {'header': {'transaction_id': '8a864b59-46e3-4c9b-8dfd-9d9a2bd4b754',
-                                  'transaction_ts': 1479264525,
-                                  'actor': 'c26dd972-8683-11e6-977b-3c970e3bee11',
-                                  'business_unit': 'a3e13076-8683-11e6-97a9-3c970e3bee11',
-                                  'create_ts': 1475180987,
-                                  'entity': 'c78f4526-8683-11e6-b1c6-3c970e3bee11',
-                                  'family_of_business': 'Test Business Family',
-                                  'line_of_business': 'My Business',
-                                  'owner': 'Test Node',
-                                  'transaction_type': 'TT_REQ'
-                                  },
-                       'payload': {'action': {'amount': '5.0', 'artifact_id': '12345', 'name': 'Test Payload'},
-                                   'source': 'f36c9086-8683-11e6-80dc-3c970e3bee11'}
-                       }
+        """ test crypto assemble_sig_block """
+        digest = "tvyb6yj6TqmmbpwiCBz9WsGmx6sOJBCvcDkw1GW5jCRWgusILKDWgn5wieDsqWEoKQtfzEgNRI4="
+        transaction = TRANSACTION.copy()
 
-        crypto.assemble_sig_block(transaction, SIGNATORY, PUBLIC_KEY, DIGEST, HASH, SIG_TS, STRIPPED_HASH)
+        crypto.assemble_sig_block(transaction, SIGNATORY, PUBLIC_KEY, digest, HASH, SIG_TS, STRIPPED_HASH)
 
         self.assertTrue('signature' in transaction and transaction['signature'] is not None, True)
         signature_block = transaction['signature']
@@ -146,20 +144,7 @@ class TestAssembleSigBlock(TestCase):
 class TestSignTransaction(TestCase):
     """ test crypto sign_transaction """
     def test_sign_transaction(self):
-        transaction = {'header': {'transaction_id': '8a864b59-46e3-4c9b-8dfd-9d9a2bd4b754',
-                                  'transaction_ts': 1479264525,
-                                  'actor': 'c26dd972-8683-11e6-977b-3c970e3bee11',
-                                  'business_unit': 'a3e13076-8683-11e6-97a9-3c970e3bee11',
-                                  'create_ts': 1475180987,
-                                  'entity': 'c78f4526-8683-11e6-b1c6-3c970e3bee11',
-                                  'family_of_business': 'Test Business Family',
-                                  'line_of_business': 'My Business',
-                                  'owner': 'Test Node',
-                                  'transaction_type': 'TT_REQ'
-                                  },
-                       'payload': {'action': {'amount': '5.0', 'artifact_id': '12345', 'name': 'Test Payload'},
-                                   'source': 'f36c9086-8683-11e6-80dc-3c970e3bee11'}
-                       }
+        transaction = TRANSACTION.copy()
 
         test_transaction = crypto.sign_transaction(SIGNATORY, PRIVATE_KEY, PUBLIC_KEY, transaction)
 
@@ -172,22 +157,9 @@ class TestSignTransaction(TestCase):
 
 
 class TestValidTransactionSig(TestCase):
-    """ test crypto valid_transaction_sig """
     def test_valid_transaction_sig(self):
-        transaction = {'header': {'transaction_id': '8a864b59-46e3-4c9b-8dfd-9d9a2bd4b754',
-                                  'transaction_ts': 1479264525,
-                                  'actor': 'c26dd972-8683-11e6-977b-3c970e3bee11',
-                                  'business_unit': 'a3e13076-8683-11e6-97a9-3c970e3bee11',
-                                  'create_ts': 1475180987,
-                                  'entity': 'c78f4526-8683-11e6-b1c6-3c970e3bee11',
-                                  'family_of_business': 'Test Business Family',
-                                  'line_of_business': 'My Business',
-                                  'owner': 'Test Node',
-                                  'transaction_type': 'TT_REQ'
-                                  },
-                       'payload': {'action': {'amount': '5.0', 'artifact_id': '12345', 'name': 'Test Payload'},
-                                   'source': 'f36c9086-8683-11e6-80dc-3c970e3bee11'}
-                       }
+        """ test crypto valid_transaction_sig """
+        transaction = TRANSACTION.copy()
 
         # sign transaction (tested prior to this call)
         test_transaction = crypto.sign_transaction(SIGNATORY, PRIVATE_KEY, PUBLIC_KEY, transaction)
@@ -199,8 +171,8 @@ class TestValidTransactionSig(TestCase):
 
 
 class TestValidateVerificationRecord(TestCase):
-    """ test crypto validate_verification_record """
     def test_validate_verification_record(self):
+        """ test crypto validate_verification_record """
         test_output = crypto.sign_verification_record(SIGNATORY, PRIOR_BLOCK_HASH, LOWER_PHASE_HASH, PUBLIC_KEY, PRIVATE_KEY, BLOCK_ID, PHASE, ORIGIN_ID,
                                                       1479435043, "")
         test_signature = test_output['verification_record']['signature']
