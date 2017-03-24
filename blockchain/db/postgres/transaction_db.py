dc9e203c3c3f7b4e7ec8f1dfceb65afaf5696bce
Steve Owens <steve.owens@disney.com>
2017-03-23 22:34:05 -0700

    Use query_helper from transaction_db.py

--- blockchain/db/postgres/transaction_db.py
+++ blockchain/db/postgres/transaction_db.py
@@ -31,13 +31,14 @@ __email__ = "joe@dragonchain.org"
 
 import psycopg2.extras
 import uuid
+import copy
 from psycopg2.extras import Json
 from blockchain.db.postgres.utilities.sql_clause_builder import SQLClauseBuilder
+from blockchain.db.postgres.utilities.sql_query_helper import SQLQueryHelper
+
 from blockchain.qry import format_transaction
 from postgres import get_connection_pool
 
-""" CONSTANTS """
-DEFAULT_PAGE_SIZE = 1000
 """ SQL Queries """
 SQL_GET_BY_ID = """SELECT * FROM transactions WHERE transaction_id = %s"""
 # adding to test querying anything
@@ -60,10 +61,8 @@ SQL_INSERT = """INSERT into transactions (
 SQL_UPDATE = """UPDATE transactions SET status = %s, block_id = %s WHERE transaction_id = %s"""
 SQL_FIXATE_BLOCK = """UPDATE transactions SET status='pending', block_id=%i WHERE status = 'new' AND transaction_ts >= to_timestamp(%i) AND transaction_ts <= to_timestamp(%i)"""
 
-
-def get_cursor_name():
-    return str(uuid.uuid4())
-
+builder = SQLClauseBuilder()
+query_helper = SQLQueryHelper()
 
 def get(self, txid):
     conn = get_connection_pool().getconn()
@@ -77,71 +76,56 @@ def get(self, txid):
         return result
     finally:
         get_connection_pool().putconn(conn)
-
-def __run_query(self, query):
-    conn = get_connection_pool().getconn()
-    try:
-        cur = conn.cursor(get_cursor_name(), cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute(query)
-        'An iterator that uses fetchmany to keep memory usage down'
-        while True:
-            results = cur.fetchmany(DEFAULT_PAGE_SIZE)
-            if not results:
-                break
-            for result in results:
-                yield format_transaction(result)
-        cur.close()
-    finally:
-        get_connection_pool().putconn(conn)
+        
     
 def get_all(self, limit = 10, offset=None, **params):
     query = SQL_GET_ALL
     if params:
         query +=  " WHERE "
-    valid_string_values = ['block_id', 'transaction_type',
+        valid_string_values = ['block_id', 'transaction_type',
                     'business_unit', 'family_of_business',
                     'line_of_business', 'signature',
                     'status', 'owner', 'actor', 'entity']
-    valid_time_range_values = ['create_ts', 'transaction_ts']
-    builder = SQLClauseBuilder("AND")
-    string_constraints = builder.build("string", valid_string_values, params)
-    time_constraints = builder.build("time_range", valid_time_range_values, params)
-    if string_constraints:
-        query += string_constraints
+        valid_time_range_values = ['create_ts', 'transaction_ts']
+    
+        string_constraints = builder.build(" AND ","string", 
+                                       valid_string_values, params)
+        time_constraints = builder.build(" AND ", "time_range", 
+                                     valid_time_range_values, params)
+        clauses = []
+        if string_constraints:
+            clauses.append(string_constraints)
         if time_constraints:
-            query += " AND " + time_constraints
-    elif time_constraints:
-        query += time_constraints
+            clauses.append(time_constraints)
+            
+        if clauses:
+            query += " AND " + " AND ".join(clauses)
+
     if params["transaction_ts"]:   
         query += " ORDER BY transaction_ts DESC "
-
     query += """ LIMIT """ + str(limit)
-
     if offset:
         query += """ OFFSET """ + str(offset)
 
-    return self.__run_query(query)
+    return query_helper.run_query(query, format_transaction)
 
 
 def get_subscription_txns(self, criteria, block_id=None):
     """ retrieve transactions that meet given criteria and have a block_id >= minimum_block_id """
-    query = SQL_GET_ALL
-    query += """ WHERE """
     if not criteria and not block_id:
         raise ValueError("No criteria specified for the query.")
-    segments = []
-    if "transaction_type" in criteria:
-        segments.append("transaction_type = %(transaction_type)s")
-    if "actor" in criteria:
-        segments.append(" actor = %(actor)s")
-    if "entity" in criteria:
-        segments.append(" entity = %(entity)s")
-    if "owner" in criteria:
-        segments.append(" owner = %(owner)s")
+    query = SQL_GET_ALL 
+    allowed_params = [ 'transaction_type', 'actor', 
+                      'entity', 'owner', 'block_id']
+    
+    params = criteria.copy()
     if block_id:
-        segments.append(" block_id = %(block_id)s")  
-    query += " AND ".join(segments)
-    return self.__run_query(query)
+        params['block_id'] = block_id
+        
+    query += " WHERE " 
+    query += builder.build_parameter_list(" AND ", allowed_params, params)
+    return query_helper.query_for_results(query, format_transaction)
+
 
 
 def insert_transaction(txn):
