1e5f4d6f13b95db3259066fe21b6657571b09178
Benedetto <benedea@students.wwu.edu>
2017-04-16 15:52:22 -0700

    Removed changes to processing.

--- blockchain/processing.py
+++ blockchain/processing.py
@@ -674,10 +674,9 @@ class ProcessingNode(object):
             verification_info['verification_records'][r['origin_id']][r['timestamp_id']] = r['signature']['hash']
 
         # takes the list of hashes to be transmitted and hashes with 256 bit to get in form to send
-        verification_hash = final_hash(hashes,type=256)
-        transaction_hash += verification_hash
+        transaction_hash += final_hash(hashes,type=256)
         # sets the hash in the verification_info structure to the hash we just generated
-        verification_info['hash'] = verification_hash
+        verification_info['hash'] = transaction_hash
 
         stamper = BitcoinTimestamper(self.service_config['bitcoin_network'], BitcoinFeeProvider())
         bitcoin_tx_id = stamper.persist(transaction_hash)
