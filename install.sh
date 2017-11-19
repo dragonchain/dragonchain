35983febdba9a53d2cdddc1f35a8137a48c0e7cf
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-19 04:48:15 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -41,7 +41,7 @@ sudo docker-compose up -d
 sudo docker-compose up -d
 cd ..
 cd scripts/
-echo "Please wait for a few moments... About to import nodes (For phase 4 node operation)"
+echo "Please wait for a few moments... About to import nodes (For phase 4 node operation!!)"
 sleep 7s
 sudo python insert_db.py --owner=blocky -p=8080 --phases=00001
 sudo python insert_db.py --owner=blocky -p=8081 --phases=00010
