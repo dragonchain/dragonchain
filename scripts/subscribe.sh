6a6e00598475b01ae76a598083e31718181c4381
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-19 17:24:30 -0600

    Create subscribe.sh

---
+++ scripts/subscribe.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+str=`cat subscription.json`
+find="XXXXXXXXX"
+replace=`date +"%s"`
+result=${str//$find/$replace}
+echo "$result"
+
+curl -H 'Accept-Encoding: gzip,deflate' -X POST http://localhost:81/transaction -d "$result"
