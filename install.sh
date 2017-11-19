857fca361532227f0176a0f5f417fbf2ccb604b8
root <root@dragonchain.c.brilliant-rhino-181204.internal>
2017-11-19 10:21:31 +0000

    Debugging

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo apt update
-sudo apt-get install openssl docker docker-compose python2.7 python-pip postgresql postgresql-server-dev-9.5
+sudo apt-get install openssl uuid docker docker-compose python2.7 python-pip postgresql postgresql-server-dev-9.5
 export PYTHONPATH=./
 sudo python setup.py install
 sudo -H pip install -r requirements.txt
@@ -9,13 +9,27 @@ sudo useradd blocky
 sudo service postgresql start
 sudo -u postgres createuser  blocky
 sudo -u postgres createdb -O blocky blockchain
+sudo -u postgres createdb -O blocky blockchain1
+sudo -u postgres createdb -O blocky blockchain2
+sudo -u postgres createdb -O blocky blockchain3
 sudo -u blocky psql -U blocky -d blockchain -a -f depl.sql
+sudo -u blocky psql -U blocky -d blockchain1 -a -f depl.sql
+sudo -u blocky psql -U blocky -d blockchain2 -a -f depl.sql
+sudo -u blocky psql -U blocky -d blockchain3 -a -f depl.sql
 sudo service postgresql stop
 cd ..
 sudo mkdir pki/
 sudo openssl ecparam -name secp224r1 -genkey -out pki/sk.pem
 sudo openssl ec -in pki/sk.pem -pubout -out pki/pk.pem
 sudo mkdir logs/
+cd ..
+cd config/
+str=`cat template.yml`
+find="XXXXXXXXX"
+replace=`uuid`
+result=${str//$find/$replace}
+echo "$result" > blockchain.yml
+cd ..
 sudo service docker start
 cd docker/
 sudo docker-compose up -d
@@ -23,9 +37,11 @@ sudo docker-compose up -d
 cd ..
 cd scripts/
 echo "Please wait for a few moments... About to import nodes"
-sleep 5s
-sudo python insert_db.py
-sleep 5s
+sleep 7s
+sudo python insert_db.py --owner=blocky -p=8080 --phases=00001
+sudo python insert_db.py --owner=blocky -p=8081 --phases=00010
+sudo python insert_db.py --owner=blocky -p=8082 --phases=00011
+sleep 7s
 cd ..
 cd docker/
 sudo docker-compose restart
