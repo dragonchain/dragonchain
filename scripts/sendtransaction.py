212a0ff460e6412a0c45aeb117bd4b2451b6a86e
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-20 06:46:02 -0600

    URL safe encoding + padding

--- scripts/sendtransaction.py
+++ scripts/sendtransaction.py
@@ -3,18 +3,25 @@ import requests
 import time
 import binascii
 import os
+from base64 import urlsafe_b64encode, urlsafe_b64decode
 from Crypto.Cipher import AES
-import base64
+from Crypto import Random
+
+
+BS = 16
+pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
+unpad = lambda s: s[:-ord(s[len(s) - 1:])]
+
+base64pad = lambda s: s + '=' * (4 - len(s) % 4)
+base64unpad = lambda s: s.rstrip("=")
 def aes_encrypt(plaintext):
     data = '../key.pem'
     key = open(data, "r")
     dragonkey = key.read()
-    print(dragonkey)
-    obj = AES.new(dragonkey, AES.MODE_CBC, os.urandom(16))
-    length = 16 - (len(plaintext) % 16)
-    plaintext += chr(length)*length
-    ciphertext = obj.encrypt(plaintext)
-    return ciphertext
+    iv = Random.new().read(BS)
+    obj = AES.new(dragonkey, AES.MODE_CFB, iv, segment_size=AES.block_size * 8)
+    ciphertext = obj.encrypt(pad(str(plaintext)))
+    return base64unpad(urlsafe_b64encode(iv + ciphertext))
 
 postpost = "http://localhost:81/transaction"
 rawdata = input("INPUT A JSON FILENAME TO SEND, IT -MAY- NEED QUOTES: ")
