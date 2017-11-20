3ca28d0fac1cdfb567ec46332de29549b95400e2
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-20 04:30:26 -0600

    Update genkey.py

--- scripts/genkey.py
+++ scripts/genkey.py
@@ -1,5 +1,5 @@
 import os
-print("PRINTING THE KEY TO THE NETWORK TO key.pem for transaction.py and transaction_svc.py"
+print("PRINTING THE KEY TO THE NETWORK TO key.pem for transaction.py and transaction_svc.py into parent directory"
 key = os.urandom(16)
-with open('key.pem','w') as f:
+with open('../key.pem','w') as f:
     f.write(key)
