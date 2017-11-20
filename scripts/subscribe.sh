eaf602d373b983e035872613dd9b4550fe947633
TheRoboKitten <jacobcmcqueen@gmail.com>
2017-11-20 03:18:42 -0600

    Delete subscribe.sh

--- scripts/subscribe.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-str=`cat subscription.json`
-find="XXXXXXXXX"
-replace=`date +"%s"`
-result=${str//$find/$replace}
-echo "$result"
-
-curl -H 'Accept-Encoding: gzip,deflate' -X POST http://localhost:81/transaction -d "$result"
