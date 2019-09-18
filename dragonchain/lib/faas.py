b29e3146f81c2ca6eb9938ab4cb0a245c38540b8
Regan Karlewicz <regan@dragonchain.com>
2019-09-18 10:03:28 -0700

    Return empty string on FileNotFoundError in get_faas_auth

--- dragonchain/lib/faas.py
+++ dragonchain/lib/faas.py
@@ -5,7 +5,6 @@ from typing import List, Dict, Optional, cast, Any
 
 import requests
 
-from dragonchain.lib import faas
 from dragonchain import exceptions
 
 FAAS_GATEWAY = os.environ.get("FAAS_LOGS_GATEWAY") or os.environ["FAAS_GATEWAY"]
@@ -17,10 +16,13 @@ def get_faas_auth() -> str:
         Returns:
             A string containing authorization for OpenFaaS.
     """
-    with open("/etc/openfaas-secret/user", "r") as file:
-        username = file.read()
-    with open("/etc/openfaas-secret/password", "r") as file:
-        password = file.read()
+    try:
+        with open("/etc/openfaas-secret/user", "r") as file:
+            username = file.read()
+        with open("/etc/openfaas-secret/password", "r") as file:
+            password = file.read()
+    except FileNotFoundError:
+        return ""
 
     return f"Basic {base64.b64encode(f'{username}:{password}'.encode('utf-8')).decode('ascii')}"
 
@@ -28,8 +30,7 @@ def get_faas_auth() -> str:
 def _get_raw_logs(contract_id: str, since: Optional[str] = None, tail: Optional[int] = 100) -> List[str]:
     """Calls openfaas /system/logs endpoint with query parameters for a specific contract"""
     query_params = cast(Dict[str, Any], {"name": f"contract-{contract_id}", "tail": tail, "since": since})
-    headers = {} if os.environ.get("FAAS_LOGS_GATEWAY") else {"Authorization": faas.get_faas_auth()}
-    response = requests.get(f"{FAAS_GATEWAY}/system/logs", params=query_params, headers=headers)
+    response = requests.get(f"{FAAS_GATEWAY}/system/logs", params=query_params, headers={"Authorization": get_faas_auth()})
     if response.status_code != 200:
         if response.status_code == 404:
             raise exceptions.NotFound("Logs not found for contract")
@@ -41,5 +42,5 @@ def _get_raw_logs(contract_id: str, since: Optional[str] = None, tail: Optional[
 
 def get_logs(contract_id: str, since: Optional[str] = None, tail: Optional[int] = 100) -> List[Dict[str, str]]:
     """Gets the raw logs from openfaas and parses the ndjson into a list of dictionaries"""
-    raw_logs = _get_raw_logs(contract_id, since, tail)
+    raw_logs = list(filter(lambda x: x != "", _get_raw_logs(contract_id, since, tail)))
     return [json.loads(log) for log in raw_logs]
