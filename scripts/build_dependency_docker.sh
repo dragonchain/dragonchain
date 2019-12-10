4ee89873aa593368e3e23244b376e96a8adb74b9
Adam <adam@adamcrowder.net>
2019-12-10 22:19:12 +0000

    Add Arm64 builds (#270)
    * arm64

--- scripts/build_dependency_docker.sh
+++ scripts/build_dependency_docker.sh
@@ -1,4 +1,20 @@
 #!/bin/sh
-# Build and push dependency container
-docker build . -f cicd/Dockerfile.dependencies -t "dragonchain/dragonchain_core_dependencies:latest" --no-cache --pull
-docker push "dragonchain/dragonchain_core_dependencies:latest"
+set -e
+
+arch="$(uname -m)"
+if [ "$arch" = "x86_64" ]; then
+    TAG="linux-amd64-latest"
+    docker build . -f cicd/Dockerfile.dependencies -t "dragonchain/dragonchain_core_dependencies:$TAG" --no-cache --pull
+elif [ "$arch" = "aarch64" ]; then
+    TAG="linux-arm64-latest"
+    docker build . -f cicd/Dockerfile.dependencies.arm64 -t "dragonchain/dragonchain_core_dependencies:$TAG" --no-cache --pull
+else
+    echo "Unknown architecture $arch"
+    exit 1
+fi
+
+docker push "dragonchain/dragonchain_core_dependencies:$TAG"
+rm -rf "$HOME/.docker/manifests/docker.io_dragonchain_dragonchain_core_dependencies-latest/"
+if docker manifest create dragonchain/dragonchain_core_dependencies:latest dragonchain/dragonchain_core_dependencies:linux-amd64-latest dragonchain/dragonchain_core_dependencies:linux-arm64-latest; then
+    docker manifest push dragonchain/dragonchain_core_dependencies:latest
+fi
