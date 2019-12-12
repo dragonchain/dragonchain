d5544e7c06515abaf59bc02439068ae6965db76a
Adam <adam@adamcrowder.net>
2019-12-12 18:15:26 +0000

    Remove broken arm7 builds (#274)

--- scripts/build_dependency_docker.sh
+++ scripts/build_dependency_docker.sh
@@ -6,8 +6,6 @@ if [ "$ARCHITECTURE" = "amd64" ]; then
     arch="x86_64"
 elif [ "$ARCHITECTURE" = "arm64" ]; then
     arch="aarch64"
-elif [ "$ARCHITECTURE" = "armv7" ]; then
-    arch="armv7"
 else
     arch="$(uname -m)"
 fi
@@ -18,9 +16,6 @@ if [ "$arch" = "x86_64" ]; then
 elif [ "$arch" = "aarch64" ]; then
     TAG="linux-arm64-latest"
     docker build . -f cicd/Dockerfile.dependencies.arm64 -t "dragonchain/dragonchain_core_dependencies:$TAG" --no-cache --pull
-elif [ "$arch" = "armv7" ] || [ "$arch" = "armv7l" ] || [ "$arch" = "armv7hl" ]; then
-    TAG="linux-armv7-latest"
-    docker build . -f cicd/Dockerfile.dependencies.armv7 -t "dragonchain/dragonchain_core_dependencies:$TAG" --no-cache --pull
 else
     echo "Unknown architecture $arch"
     exit 1
@@ -28,6 +23,6 @@ fi
 
 docker push "dragonchain/dragonchain_core_dependencies:$TAG"
 rm -rf "$HOME/.docker/manifests/docker.io_dragonchain_dragonchain_core_dependencies-latest/"
-if docker manifest create dragonchain/dragonchain_core_dependencies:latest dragonchain/dragonchain_core_dependencies:linux-amd64-latest dragonchain/dragonchain_core_dependencies:linux-arm64-latest dragonchain/dragonchain_core_dependencies:linux-armv7-latest; then
+if docker manifest create dragonchain/dragonchain_core_dependencies:latest dragonchain/dragonchain_core_dependencies:linux-amd64-latest dragonchain/dragonchain_core_dependencies:linux-arm64-latest; then
     docker manifest push dragonchain/dragonchain_core_dependencies:latest
 fi
